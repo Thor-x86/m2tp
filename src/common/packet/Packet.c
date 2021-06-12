7b2edff477b75e968ae01287aa335fb62714eb7d
Athaariq Ardhiansyah <athaariqa@gmail.com>
2021-06-12 18:31:21 +0700

    feat: Add implementation for Packet

--- src/common/packet/Packet.c
+++ src/common/packet/Packet.c
@@ -3,16 +3,52 @@
  * @brief Implements Packet.h functionality
 **/
 
+#include <stdlib.h>
+
 #include "Packet.h"
 
 m2tp_bytes Packet_serialize(const Packet *input, unsigned short *outputSizePtr)
 {
-  // TODO: Write stuffs here...
-  // return ...;
+  // Resolve output size
+  unsigned short outputSize = input->contentSize + 3;
+
+  // Send output size via pointer, if not NULL
+  if (outputSizePtr != NULL)
+    *outputSizePtr = outputSize;
+
+  // Allocate memory for output
+  m2tp_bytes output = (m2tp_bytes)malloc(outputSize);
+
+  // Insert command
+  output[0] = input->command;
+
+  // Insert contentSize, as Big Endian bytes
+  output[1] = (m2tp_byte)((input->contentSize >> 8) & 0xFFU);
+  output[2] = (m2tp_byte)(input->contentSize & 0xFFU);
+
+  // Insert raw content
+  for (unsigned short i = 0; i < input->contentSize; i++)
+    output[i + 3] = input->content[i];
+
+  return output;
 }
 
 void Packet_parse(const m2tp_bytes input, Packet *output)
 {
-  // TODO: Write stuffs here...
-  // return ...;
+  // FIXME: Technique to check valid .contentSize here
+
+  // Insert command
+  output->command = input[0];
+
+  // Insert contentSize
+  output->contentSize = 0U;
+  output->contentSize |= ((unsigned short)input[1]) << 8;
+  output->contentSize |= (unsigned short)input[2];
+
+  // Allocate RAM for content
+  output->content = (m2tp_bytes)malloc(output->contentSize);
+
+  // Insert content
+  for (unsigned short i = 0; i < output->contentSize; i++)
+    output->content[i] = input[i + 3];
 }
