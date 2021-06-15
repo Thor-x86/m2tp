4e706f5a2599b0d7bffa47f2de28012245e7e4df
Athaariq Ardhiansyah <athaariqa@gmail.com>
2021-06-15 11:27:24 +0700

    refactor: Reserve 0 as success
    In a few cases, we have to return success sign via error code.

--- include/m2tp-common/errors.h
+++ include/m2tp-common/errors.h
@@ -9,51 +9,51 @@
 /**
  * @brief Thrown when currently not connected to network
  */
-#define M2TP_ERROR_NOT_CONNECTED 0
+#define M2TP_ERROR_NOT_CONNECTED 1
 
 /**
  * @brief Intentional transmit failure
  */
-#define M2TP_ERROR_TRANSMISSION_ABORT 1
+#define M2TP_ERROR_TRANSMISSION_ABORT 2
 
 /**
  * @brief Targeted address not exist, thrown from leader
  */
-#define M2TP_ERROR_ADDRESS_NOT_EXIST 2
+#define M2TP_ERROR_ADDRESS_NOT_EXIST 3
 
 /**
  * @brief Targeted topic not exist, thrown from leader
  */
-#define M2TP_ERROR_TOPIC_NOT_EXIST 3
+#define M2TP_ERROR_TOPIC_NOT_EXIST 4
 
 /**
  * @brief Data loss happened, thrown from receiver to transmitter and receiver itself
  */
-#define M2TP_ERROR_PACKET_SIZE_MISMATCH 4
+#define M2TP_ERROR_PACKET_SIZE_MISMATCH 5
 
 /**
  * @brief When app gives incorrect data size
  */
-#define M2TP_ERROR_DATA_SIZE_MISMATCH 5
+#define M2TP_ERROR_DATA_SIZE_MISMATCH 6
 
 /**
  * @brief CRC/MD5 failure, if driver supports CRC or MD5 check
  */
-#define M2TP_ERROR_DATA_CORRUPT 6
+#define M2TP_ERROR_DATA_CORRUPT 7
 
 /**
  * @brief Thrown if data size bigger than 64KB
  */
-#define M2TP_ERROR_DATA_SIZE_TOO_BIG 7
+#define M2TP_ERROR_DATA_SIZE_TOO_BIG 8
 
 /**
  * @brief One network can only holds 1 leader and 126 members
  */
-#define M2TP_ERROR_TOO_MUCH_ADDRESS 8
+#define M2TP_ERROR_TOO_MUCH_ADDRESS 9
 
 /**
  * @brief One network can only holds 127 topics
  */
-#define M2TP_ERROR_TOO_MUCH_TOPIC 9
+#define M2TP_ERROR_TOO_MUCH_TOPIC 10
 
 #endif // #ifndef IS_M2TP_ERRORS_DEFINED
