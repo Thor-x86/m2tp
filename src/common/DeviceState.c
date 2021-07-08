da259a5668c0af10c5dee3930309ae5d11abfda1
Athaariq Ardhiansyah <athaariqa@gmail.com>
2021-07-08 22:59:45 +0700

    refactor: Better naming and topic implementation

--- src/common/DeviceState.c
+++ src/common/DeviceState.c
@@ -10,12 +10,13 @@ char *DeviceState_deviceBrand = "generic";
 char *DeviceState_deviceVendor = "generic";
 char *DeviceState_deviceClass = "generic";
 m2tp_channel DeviceState_assignedAddress = NULL;
-// Variables:
-bool driverReady = false;
-char *deviceBrand = "generic";
-char *deviceVendor = "generic";
-char *deviceClass = "generic";
-m2tp_channel assignedAddress = NULL;
+m2tp_TopicListener DeviceState_subscribedTopics[128];
+
+void DeviceState_resetTopics()
+{
+  for (m2tp_byte i = 0; i < 128; i++)
+    DeviceState_subscribedTopics[i] = NULL;
+}
 
 // NOTE: Other functions are implemented at member and leader
 //       because they're work differently
--- src/common/DeviceState.h
+++ src/common/DeviceState.h
@@ -16,9 +16,27 @@ extern char *DeviceState_deviceVendor;
 extern char *DeviceState_deviceClass;
 
 // NULL means not assigned or as leader
-extern m2tp_channel assignedAddress;
+extern m2tp_channel DeviceState_assignedAddress;
+
+// Key-value map for "(TopicID-128) => Listener"
+extern m2tp_TopicListener DeviceState_subscribedTopics[128];
+
+// Clear and initialize topic subscription
+extern void DeviceState_resetTopics();
 
 // Check if current device is ready to communicate
 extern bool DeviceState_isReady();
+
+// Syntactic sugar for subscribing a listener function to specific topicID
+#define DeviceState_assignTopic(topicID, listener) (DeviceState_subscribedTopics[topicID - 128U] = listener)
+
+// Syntactic sugar for unsubscribe a topic
+#define DeviceState_unassignTopic(topicID) (DeviceState_subscribedTopics[topicID - 128U] = NULL)
+
+// Syntactic sugar for checking if topic is assigned or not
+#define DeviceState_isTopicAssigned(topicID) (DeviceState_subscribedTopics[topicID - 128U] != NULL)
+
+// Syntactic sugar for calling topic listener
+#define DeviceState_callTopicListener(topicID, dataSize, data) (DeviceState_subscribedTopics[topicID - 128U](dataSize, data))
 
 #endif // #ifndef IS_DEVICE_STATE_DEFINED
